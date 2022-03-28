344dcf9f52eb9f0b0340e9ac451f42af9a24a563
eternal <eternal.git@gmail.com>
2022-03-28 19:22:27 +0100

    cleanup

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ import Plugin from '@structures/plugin';
 import { getByProps } from '@webpack';
 import Commands from '@api/commands';
 
-const [
-   Channels,
-   Unreads,
-   Messages
-] = getByProps(
-   ['getDMFromUserId'],
-   ['ack', 'ackCategory'],
-   ['hasUnread', 'lastMessageId'],
-   { bulk: true }
-);
-
 export default class MarkAllRead extends Plugin {
    start() {
+      const [
+         Channels,
+         Unreads,
+         Messages
+      ] = getByProps(
+         ['getDMFromUserId'],
+         ['ack', 'ackCategory'],
+         ['hasUnread', 'lastMessageId'],
+         { bulk: true }
+      );
+
+
       Commands.register({
          command: 'read',
          description: 'Marks all channels as read.',
          execute: async () => {
-            const channels = Object.keys(Channels.getMutableGuildAndPrivateChannels());
+            const mutable = Channels.getMutableGuildAndPrivateChannels();
+            const channels = Object.keys(mutable);
 
             const unreads = channels.map(c => ({
                channelId: c,
