da09fd47c227e3f037df9c3b8d20d44001516393
eternal <eternal.git@gmail.com>
2022-04-05 07:58:15 +0100

    fix

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ export default class MarkAllRead extends Plugin {
          { bulk: true }
       );
 
-
       Commands.register({
          command: 'read',
          description: 'Marks all channels as read.',
          execute: async () => {
-            const mutable = Channels.getMutableGuildAndPrivateChannels();
+            const mutable = Channels.loadAllGuildAndPrivateChannelsFromDisk();
             const channels = Object.keys(mutable);
 
             const unreads = channels.map(c => ({
